de3b010e254dee7152ce787d00409e17198ef84a
fferraro <ferraro@cs.jhu.edu>
2015-02-13 18:05:44 -0500

    1. Added version number to docs 2. concrete_fileinfo.js -> concrete_info.js

--- doc/regenerate_docs.sh
+++ doc/regenerate_docs.sh
@@ -24,12 +24,18 @@ for f in $(find gen-html -type f -name '*.html' ); do
     tail -n +5 $f | python reorder_divs.py >> schema/$(basename $f)
 done
 
-# Create JavaScript file with array of Thrift types
-CONCRETE_FILELIST="schema/concrete_filelist.js"
-echo "var CONCRETE_FILELIST = [" > ${CONCRETE_FILELIST}
+# Create JavaScript file with Concrete information
+CONCRETE_INFO="schema/concrete_info.js"
+# version
+echo -n "var CONCRETE_VERSION = " > ${CONCRETE_INFO};
+CONCRETE_README_PATH="../README.md"
+echo -n $(grep -E 'Current version:\s*`' ${CONCRETE_README_PATH} | perl -pe 's/.*(\d+\.\d*).*/$1/') >> ${CONCRETE_INFO}
+echo ";" >> ${CONCRETE_INFO}
+# array of Thrift types
+echo "var CONCRETE_FILELIST = [" >> ${CONCRETE_INFO}
 for f in $(find "${THRIFT_FILES}" -type f -name '*.thrift' | sort); do 
     filename=$(basename $f)
     filename_without_prefix="${filename%.*}"
-    echo "'${filename_without_prefix}'," >> ${CONCRETE_FILELIST}
+    echo "'${filename_without_prefix}'," >> ${CONCRETE_INFO}
 done
-echo "];" >> ${CONCRETE_FILELIST}
+echo "];" >> ${CONCRETE_INFO}
