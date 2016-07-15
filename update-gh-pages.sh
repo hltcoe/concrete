8366f80bc851fd881d1519f237b878386fd882f7
max thomas <max@maxthomas.io>
2016-07-15 12:23:15 -0400

    OK

--- update-gh-pages.sh
+++ update-gh-pages.sh
@@ -8,7 +8,7 @@ git fetch github
 git checkout -b gh-pages github/gh-pages
 cd doc
 sh regenerate_docs.sh $(which thrift) || $(echo "No thrift found."; exit 2)
-mv schema/* $STARTDIR/
+mv -f schema/* $STARTDIR/
 cd $STARTDIR
 git commit . -m "update gh-pages"
 git push github gh-pages || $(echo "Push failed, check ssh key/rights?"; exit 1)
